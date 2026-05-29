3b4875c2fc3858db86cb4ab427cc1879bbba918b
unknown <jonathansetya01@gmail.com>
2026-05-29 13:04:51 +0700

    Fix API sama benerin manggilnya

---
+++ prediksi_banjir/notification.py
@@ -0,0 +1,43 @@
+import requests
+import json
+import streamlit as st
+
+ONESIGNAL_APP_ID = st.secrets["ONESIGNAL_APP_ID"]
+REST_API_KEY = st.secrets["REST_API_KEY"]
+
+def send_notification(title, message):
+
+    headers = {
+        "Content-Type": "application/json; charset=utf-8",
+        "Authorization": f"Basic {REST_API_KEY}"
+    }
+
+    payload = {
+        "app_id": ONESIGNAL_APP_ID,
+
+        "included_segments": ["All"],
+
+        "headings": {
+            "en": title
+        },
+
+        "contents": {
+            "en": message
+        },
+
+        "priority": 10
+    }
+
+    response = requests.post(
+        "https://onesignal.com/api/v1/notifications",
+        headers=headers,
+        data=json.dumps(payload)
+    )
+
+    print("Status:", response.status_code)
+    print(response.text)
+
+    if response.status_code == 200:
+        return True
+    else:
+        return False
